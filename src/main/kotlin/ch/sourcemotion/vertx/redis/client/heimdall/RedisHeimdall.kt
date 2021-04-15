1699afd6644f74e47c41ab41b3d57af85bdd3c4e
Michel Werren <michel.werren@source-motion.ch>
2021-04-15 20:56:17 +0200

    chore: Improve Javadoc

--- src/main/kotlin/ch/sourcemotion/vertx/redis/client/heimdall/RedisHeimdall.kt
+++ src/main/kotlin/ch/sourcemotion/vertx/redis/client/heimdall/RedisHeimdall.kt
@@ -7,12 +7,21 @@ import io.vertx.redis.client.Redis
 /**
  * Redis client for common use. Additionally reconnection is handled automatically.
  * Please visit README for more detailed information.
+ *
+ * This extends the original Vert.x Redis client.
+ * See [io.vertx.redis.client.Redis]
  */
 interface RedisHeimdall : Redis, AutoCloseable {
     companion object {
+        /**
+         * Creates the common Redis Heimdall client.
+         */
         @JvmStatic
         fun create(vertx: Vertx, options: RedisHeimdallOptions): RedisHeimdall = RedisHeimdallImpl(vertx, options)
 
+        /**
+         * Creates the light Redis Heimdall client. That client will use only a single connection to Redis.
+         */
         @JvmStatic
         fun createLight(vertx: Vertx, options: RedisHeimdallOptions): RedisHeimdall = RedisHeimdallLight(vertx, options)
     }
--- src/main/kotlin/ch/sourcemotion/vertx/redis/client/heimdall/subscription/SubscriptionMessage.kt
+++ src/main/kotlin/ch/sourcemotion/vertx/redis/client/heimdall/subscription/SubscriptionMessage.kt
@@ -1,3 +1,6 @@
 package ch.sourcemotion.vertx.redis.client.heimdall.subscription
 
+/**
+ * Message that will received by [RedisHeimdallSubscription]
+ */
 data class SubscriptionMessage(val channel: String, val message: String)
